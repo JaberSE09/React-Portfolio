beac15f1a48b095b8ecc647d914f6b7201236454
Sajid Jaber <sajidjaber@yahoo.com>
2022-07-17 10:01:47 -0500

    routes fixed

--- src/components/NavBarLinks.js
+++ src/components/NavBarLinks.js
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 function NavBarLinks() {
     return (
         <Nav className="me-auto">
-        <Nav.Link className="text-white" as={Link} to="/React-Portfolio">About Me</Nav.Link>
+        <Nav.Link className="text-white" as={Link} to="/">About Me</Nav.Link>
         <Nav.Link className="text-white" as={Link} to="/contact">Contact Me</Nav.Link>
         <Nav.Link className="text-white" as={Link} to="/portfolio">Portfolio</Nav.Link>
         <Nav.Link className="text-white" as={Link} to="/resume">Resume</Nav.Link>
