7218f2ed2dd2c5c2df9a7cc7f288830674c36dbb
Sajid Jaber <sajidjaber@yahoo.com>
2022-07-23 19:39:03 -0500

    fixed the projects and resume sections

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,13 @@
 import "./Footer.css"
+import {DiGithubAlt} from "react-icons/di"
+import {AiFillLinkedin} from "react-icons/ai"
 
 function Footer() {
   return (
     <footer className="footer bg-dark text-white">
         <div className="text-center p-3">© 2022 Copyright Sajid Jaber
-        <div className="btn"><a href="https://github.com/JaberSE09">Github</a></div>
-        <div className="btn"><a href="https://www.linkedin.com/in/sajidjaber/">Linkedin</a></div>
+        <div className="btn"><a href="https://github.com/JaberSE09"><DiGithubAlt /></a></div>
+        <div className="btn"><a href="https://www.linkedin.com/in/sajidjaber/"><AiFillLinkedin /></a></div>
         </div>
         </footer>
   );
