a226ffce1e56c190107f540227f5b05349a4ce2f
Sajid Jaber <sajidjaber@yahoo.com>
2022-07-25 05:17:51 -0500

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,24 @@
-import "./Footer.css"
-import {DiGithubAlt} from "react-icons/di"
-import {AiFillLinkedin} from "react-icons/ai"
+import "./Footer.css";
+import { DiGithubAlt } from "react-icons/di";
+import { AiFillLinkedin } from "react-icons/ai";
 
 function Footer() {
   return (
     <footer className="footer bg-dark text-white">
-        <div className="text-center p-3">© 2022 Copyright Sajid Jaber
-        <div className="btn"><a href="https://github.com/JaberSE09"><DiGithubAlt /></a></div>
-        <div className="btn"><a href="https://www.linkedin.com/in/sajidjaber/"><AiFillLinkedin /></a></div>
+      <div className="text-center p-3">
+        © 2022 Copyright Sajid Jaber
+        <div className="btn">
+          <a href="https://github.com/JaberSE09">
+            <DiGithubAlt size={30} />
+          </a>
         </div>
-        </footer>
+        <div className="btn">
+          <a href="https://www.linkedin.com/in/sajidjaber/">
+            <AiFillLinkedin size={30} />
+          </a>
+        </div>
+      </div>
+    </footer>
   );
 }
 
