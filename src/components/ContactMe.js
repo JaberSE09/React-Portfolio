7953ef86dad4f8a26cf43857ae7e513872684949
Sajid Jaber <sajidjaber@yahoo.com>
2022-07-25 05:17:06 -0500

    contact me

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,12 +1,88 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faPhone,
-  faMailReply,
-  faMapMarked,
-} from "@fortawesome/free-solid-svg-icons";
+import { AiFillMail, AiFillMessage } from "react-icons/ai";
+import { IoMdPerson } from "react-icons/io";
+
 
 function ContactMe() {
   return (
+    <div style={{ backgroundColor: "#151f20" }}>
+      <h2 class="text-center">Contact Form</h2>
+      <div class="row justify-content-center">
+        <div class="col-12 col-md-8 col-lg-6 pb-5">
+          <form method="post">
+            <div class="card border-primary rounded-0">
+              <div class="card-header p-0">
+                <div class="bg-info text-white text-center py-2">
+                  <h3>Contact Me</h3>
+                  <p class="m-0">Send A Message</p>
+                </div>
+              </div>
+              <div class="card-body p-3">
+                <div class="form-group">
+                  <div class="input-group mb-2">
+                    <div class="input-group-prepend">
+                      <div class="input-group-text">
+                        <IoMdPerson />
+                      </div>
+                    </div>
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="name"
+                      name="nombre"
+                      placeholder="Name"
+                      required
+                    />
+                  </div>
+                </div>
+                <div class="form-group">
+                  <div class="input-group mb-2">
+                    <div class="input-group-prepend">
+                      <div class="input-group-text">
+                        <AiFillMail />
+                      </div>
+                    </div>
+                    <input
+                      type="email"
+                      class="form-control"
+                      id="email"
+                      name="email"
+                      placeholder="example@gmail.com"
+                      required
+                    />
+                  </div>
+                </div>
+
+                <div class="form-group">
+                  <div class="input-group mb-2">
+                    <div class="input-group-prepend">
+                      <div class="input-group-text">
+                        <AiFillMessage />
+                      </div>
+                    </div>
+                    <textarea
+                      id="message"
+                      class="form-control"
+                      placeholder="this is a message..."
+                      required
+                    ></textarea>
+                  </div>
+                </div>
+
+                <div class="text-center">
+                  <input
+                    type="submit"
+                    value="Submit"
+                    class="btn btn-info btn-block rounded-0 py-2"
+                  />
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+
+    /* 
     <div classNameName="contact">
       <h2 className="h1-responsive font-weight-bold text-center my-4">
         Contact Me
@@ -104,7 +180,7 @@ function ContactMe() {
       </div>
    
     </div> 
-  </div>
+  </div> */
   );
 }
 
